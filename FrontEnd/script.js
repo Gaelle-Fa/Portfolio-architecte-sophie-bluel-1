6cab849d64cfd740c686c42cba5d9cb6824a2e58
Gaelle-Fa <126414422+Gaelle-Fa@users.noreply.github.com>
2023-12-26 13:46:09 +0100

    categories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,24 +1,41 @@
-function fetchMarks(){
+fetch("http://localhost:5678/api/works")
+    .then( Response) = Response.json()
+    .then((works) => displayWorks (works))
+    
+     
+    function displayWorks (works) {
+        for (let index = 0; index < works.lenght; index++){
+            const worksIndex = works [index];
+            const sectionGallery =document.querySelector(".gallery");
+            const worksElement = document.createElement("figure");
+            const imageElement = document.createElement("img");
+                imageElement.src = worksIndex.imageUrl;
+                imageElement.alt = worksIndex.title;
+            const titleElement = document.createElement("h3");
+                titleElement.innerHTML = worksIndex.title;
+
+            sectionGallery.appendChild(worksElement);
+
+            worksElement.appendChild(imageElement);
+            worksElement.appendChild(titleElement);
+        }
+    }
 
 
-fetch(`http://localhost:5678/api/works`).then((res) => {
-        res.json().then((data) => {
-            data.forEach(element => {
-                console.log(element)
+    
+fetch("http://localhost:5678/api/categories")
+    .then((response)=>response.json())
+    .then((categories)=>displayCategory(categories))
 
-                const figure = document.createElement(`figure`)
-                const image = document.createElement(`img`)
-                const figureCaption = document.createElement(`figcaption`)
-
-                figure.appendChild(image)
-                figure.appendChild(figureCaption)
-
-                figureCaption.innerHTML = elem.title
-                image.src = elem.imageUrl
-
-                document.querySelector(`.gallery`).appendChild(figure)
-            }); 
-        })
-})
-
-}
+    function displayCategory (categories)  {
+        const sectionFiltres = document.querySelector("filtres");
+        const buttonAll = document.createElement("button");
+            buttonAll.textContent = "tous";
+            buttonAll.classList.add("btnFilter");
+            sectionFiltres.appendChild(buttonAll);
+    
+    
+    
+    }
+    fetchWorks()
+    fetchCategories()
